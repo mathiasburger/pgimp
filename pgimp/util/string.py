ec591bf83d119657ec98a4257889f5f6d5cfaa03
Mathias Burger <mathias.burger@gmail.com>
2018-11-02 15:48:32 +0100

    Also evaluate doctests in tests.

--- pgimp/util/string.py
+++ pgimp/util/string.py
@@ -1,2 +1,12 @@
 def escape_single_quotes(string: str):
+    """
+    Escapes single quotes. Can be used when generating python code that places string contents into single quotes.
+
+    Example:
+
+    >>> print("print('{:s}')".format(escape_single_quotes("'")))
+    print('\\'')
+
+    :return:
+    """
     return string.replace("'", "\\'")
