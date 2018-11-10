14e50a64adbf9e3449b138ca875e632bdbbc129b
Mathias Burger <mathias.burger@gmail.com>
2018-11-10 15:47:43 +0100

    Fix execution order.

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ from pgimp.doc.output.OutputPythonSkeleton import OutputPythonSkeleton
 from pgimp.util import file
 from pgimp import __version__, project
 
+
+generate_python_skeleton = GimpDocumentationGenerator(OutputPythonSkeleton(
+   file.relative_to(__file__, 'gimp'))
+)
+generate_python_skeleton()
+
 setup(name=project,
       version=__version__,
       description='Call gimp routines from python3 code.',
@@ -14,9 +20,4 @@ setup(name=project,
       license='MIT',
       packages=[project, 'gimp', 'gimpenums', 'gimpfu'],
       zip_safe=False
-  )
-
-generate_python_skeleton = GimpDocumentationGenerator(OutputPythonSkeleton(
-   file.relative_to(__file__, 'gimp'))
 )
-generate_python_skeleton()
