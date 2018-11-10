c0bd1e2a0cd1242059aa855a99b913c2c46ce62e
Mathias Burger <mathias.burger@gmail.com>
2018-11-10 15:50:14 +0100

    Style.

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ from pgimp.doc.output.OutputPythonSkeleton import OutputPythonSkeleton
 from pgimp.util import file
 from pgimp import __version__, project
 
-
 generate_python_skeleton = GimpDocumentationGenerator(OutputPythonSkeleton(
    file.relative_to(__file__, 'gimp'))
 )
 generate_python_skeleton()
 
-setup(name=project,
-      version=__version__,
-      description='Call gimp routines from python3 code.',
-      url='https://github.com/mabu-github/pgimp',
-      author='Mathias Burger',
-      author_email='mathias.burger@gmail.com',
-      license='MIT',
-      packages=[project, 'gimp', 'gimpenums', 'gimpfu'],
-      zip_safe=False
+setup(
+    name=project,
+    version=__version__,
+    description='Call gimp routines from python3 code.',
+    url='https://github.com/mabu-github/pgimp',
+    author='Mathias Burger',
+    author_email='mathias.burger@gmail.com',
+    license='MIT',
+    packages=[project, 'gimp', 'gimpenums', 'gimpfu'],
+    zip_safe=False
 )
