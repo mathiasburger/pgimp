76663c29af8f7df1f62ba00a03056763205759fb
Mathias Burger <mathias.burger@gmail.com>
2019-01-10 12:31:56 +0100

    Update tagging script.

--- tag.sh
+++ tag.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+project_root=$(dirname $(readlink -f $0))
+
 tag_name="$1"
+
+echo ">>> prepare release <<<"
+version_file="${project_root}/pgimp/__init__.py"
+sed -i'' "s/__version__ = .*/__version__ = '$1'/" "${version_file}"
+git commit -m "Prepare release $1." "${version_file}"
+git push
+
+echo ">>> create tag <<<"
 git tag -am "v$1" "v$1"
 git push origin "v$1"
