fa76ef5a8c97eb178410b7ca54ce9db4bde999f8
Mathias Burger <mathias.burger@gmail.com>
2019-02-08 18:32:23 +0100

    Check python2 installation.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
+import re
 import shutil
+import subprocess
 
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
@@ -7,6 +9,37 @@ from setuptools.command.build_py import build_py
 import pgimp
 from pgimp import __version__, PROJECT, AUTHOR
 from pgimp.util import file
+
+
+class GimpInstallationException(Exception):
+    pass
+
+
+def check_python2_installation():
+    python = shutil.which('python2')
+    if python is None:
+        raise GimpInstallationException('Could not find a python2 installation.')
+    proc = subprocess.Popen(
+        [python],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    requirements = open(os.path.join(os.path.dirname(__file__), 'requirements-python2.txt'), 'r').readlines()
+    imports = list(map(lambda r: re.split('[<=>\\s]', r)[0], filter(lambda x: x != '\n', requirements)))
+    import_statements = list(map(lambda r: 'import ' + r, imports))
+    stdout, stderr = proc.communicate(
+        '\n'.join(import_statements).encode(),
+        timeout=5
+    )
+    if stderr.decode() != '':
+        raise GimpInstallationException(
+            'At least one of the following packages is missing in the python2 installation: ' + ', '.join(imports)
+        )
+
+
+check_python2_installation()
 
 pgimp.execute_scripts_with_process_check = False  # because psutil might not yet be installed
 
